9fe040ffbf965c1f7ecacc2758fdf9e754a4c205
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-05-13 14:22:36 -0700

    Created helper functions for scheme database.

--- queries.sql
+++ queries.sql
@@ -2,7 +2,12 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+    SELECT product.ProductName, category.CategoryName FROM "product"
+    JOIN "category" ON product.CategoryId = category.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+    
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
